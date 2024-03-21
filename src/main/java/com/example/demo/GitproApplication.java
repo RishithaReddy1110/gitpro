0351edc5655fa651fa3ee708eb6624e74edc1dd5
mrishith <mrishith@LIN-C08WBK3.corp.capgemini.com>
2024-03-21 21:09:37 +0530

    first commit

---
+++ src/main/java/com/example/demo/GitproApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitproApplication.class, args);
+	}
+
+}
